2907fafb82ac6e7c097c055fe77d83fd0803d3af
Iliya Svetlichnii <batya2095@gmail.com>
2018-09-13 11:37:11 +0600

    add notification

---
+++ lib/temirtulpar_helper/services/notification.rb
@@ -0,0 +1,32 @@
+module TemirtulparHelper
+  module Services
+    class Notification
+      attr_reader :id, :type
+
+      def initialize(id, type, msg)
+        @id = id
+        @type = type
+        @msg = msg
+      end
+
+      def send
+        uri = URI.parse("http://ntf.temirtulpar.com/snd")
+        request = Net::HTTP::Post.new(uri)
+        request.content_type = "application/json"
+        request.body = JSON.dump({
+          "type" => @type,
+          "id" => @id,
+          "msg" => @msg
+        })
+
+        req_options = {
+          use_ssl: uri.scheme == "https",
+        }
+
+        Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+          http.request(request)
+        end
+      end
+    end
+  end
+end
