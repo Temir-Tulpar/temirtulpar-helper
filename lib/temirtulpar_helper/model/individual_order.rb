6e491b9eb5014937c5eef2f7aed8d92b61c2e981
Iliya Svetlichnii <batya2095@gmail.com>
2018-10-19 10:59:40 +0600

    update order statuses

--- lib/temirtulpar_helper/model/individual_order.rb
+++ lib/temirtulpar_helper/model/individual_order.rb
@@ -1,7 +1,7 @@
 module TemirtulparHelper
   module Model
     module IndividualOrder
-      STATUSES = %i[pending_payment paid in_progress canceled finished].freeze
+      STATUSES = %i[pending_payment paid in_progress canceled finished proof_payment payment_error].freeze
       DELIVERY_METHODS = [:delivery, :pick_up].freeze
       PAYMENT = [:cash, :bank, :card].freeze
       PERSON_TYPE = [:individual, :legal].freeze
