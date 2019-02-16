8027884f516e4a213d874d8b9e2a8fd0c06e2dc5
ilia <piryazevilia@gmail.com>
2019-02-16 19:25:49 +0600

    fix notification service

--- lib/temirtulpar_helper/services/notification.rb
+++ lib/temirtulpar_helper/services/notification.rb
@@ -1,7 +1,9 @@
+require 'net/http'
+
 module TemirtulparHelper
   module Services
     class Notification
-      attr_reader :id, :type
+      attr_reader :id, :type, :msg
 
       def initialize(id, type, msg)
         @id = id
@@ -10,18 +12,16 @@ module TemirtulparHelper
       end
 
       def send
-        uri = URI.parse("http://ntf.temirtulpar.com/snd")
+        uri = URI.parse('http://ntf.temirtulpar.com/snd')
         request = Net::HTTP::Post.new(uri)
-        request.content_type = "application/json"
+        request.content_type = 'application/json'
         request.body = JSON.dump({
-          "type" => @type,
-          "id" => @id,
-          "msg" => @msg
+          type: type,
+          id: id,
+          msg: msg
         })
 
-        req_options = {
-          use_ssl: uri.scheme == "https",
-        }
+        req_options = {use_ssl: uri.scheme == 'https'}
 
         Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
           http.request(request)
